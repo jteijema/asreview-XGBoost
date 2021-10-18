01c3fac0db33f1343f4b40ef1c76ac83eb271b83
Jelle Teijema <j.j.teijema@gmail.com>
2021-10-18 17:53:57 +0200

    Update xgboost.py

--- asreviewcontrib/models/xgboost.py
+++ asreviewcontrib/models/xgboost.py
@@ -2,35 +2,21 @@ from asreview.models.classifiers.base import BaseTrainClassifier
 
 import xgboost as xgb
 
-import numpy as np
-
 class XGBoost(BaseTrainClassifier):
     """XGBoost classifier
+
+    This classifier is based on the XGBClassifier.
 
     """
 
     name = "xgboost"
 
     def __init__(self,
-                 objective='multi:softmax',
-                 num_class = 2,
-                 eval_metric = "logloss",
-                 colsample_bytree = 0.3, 
-                 learning_rate = 0.1,
-                 max_depth = 5, 
-                 alpha = 10, 
-                 n_estimators = 10):
+                 eval_metric = "logloss",):
 
         super().__init__()
 
-        self.objective=objective
-        self.num_class = num_class
         self.eval_metric = eval_metric
-        self.colsample_bytree = colsample_bytree
-        self.learning_rate = learning_rate
-        self.max_depth = max_depth
-        self.alpha = alpha
-        self.n_estimators = n_estimators
-                
-        self._model = xgb.XGBClassifier(eval_metric = eval_metric)
+
+        self._model = xgb.XGBClassifier(eval_metric = eval_metric, use_label_encoder=False)
 
